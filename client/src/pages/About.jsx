4e105c0e53d93fd3dd6d23196b36ee342fdd0fca
Ankesh-287 <ankeshpatil287@gmail.com>
2025-05-31 10:39:36 +0530

    about texts

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -12,13 +12,10 @@ const AboutPage = () => {
         </Typography>
         <Divider sx={{ width: 60, mx: 'auto', mb: 3 }} />
         <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
-          Proin eu ante vel mauris molestie dignissim non eget nunc. Integer ac massa orci.
-          Suspendisse vulputate semper nunc eget rhoncus.
+         A better way to talk with your customers 
         </Typography>
         <Typography variant="body1" color="text.secondary">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu ante vel mauris molestie
-          dignissim non eget nunc. Integer ac massa orci. Suspendisse vulputate semper nunc eget
-          rhoncus. Ut sit amet porta sem, interdum tincidunt libero.
+          We help people achieve independence by making it easier to start, run, and grow a business. We believe the future of commerce has more voices, not fewer, so we're reducing the barriers to business ownership to make commerce better for everyone. 
         </Typography>
       </Container>
 
@@ -71,9 +68,7 @@ const AboutPage = () => {
 
           <Grid item xs={12} md={6}>
             <Typography variant="h6" fontWeight="700" gutterBottom sx={{ pb: 2 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu ante vel mauris
-              molestie dignissim non eget nunc. Integer ac massa orci. Suspendisse vulputate
-              semper nunc eget rhoncus. Aenean placerat facilisis ex, eu laoreet lorem convallis.
+              Kal has grown from 5 people in a coffee shop to over 5,000 across the globe. With over 1,000,000 businesses powered by Shopify, we care deeply about the work we do. We're constant learners who thrive on change and seek to have an impact in everything we do.
             </Typography>
             <Typography variant="h6" fontWeight="bold" gutterBottom>
               Fusce gravida justo a lectus tempus lacinia. Ut mollis scelerisque ultricies. Aenean
@@ -89,23 +84,20 @@ const AboutPage = () => {
         </Typography>
         <Divider sx={{ width: 60, mx: 'auto', mb: 3 }} />
         <Typography variant="body1" textAlign="center" color="text.secondary" mb={5}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
-          ullamcorper mattis, pulvinar dapibus leo.
+          We believe we can all make a difference. 
         </Typography>
         <Grid container spacing={4} sx={{ boxSizing: 'border-box', alignItems: 'stretch' }}>
           <Grid item xs={12} md={6}>
             <Box sx={{ backgroundColor: '#fbeeee', p: { xs: 4, md: 10 }, height: '100%' }}>
-              <Typography variant="h4" fontWeight="600" gutterBottom>
-                Vel mauris molestie dignissim
+              <Typography variant="h5" fontWeight="600" gutterBottom sx={{mb:2}}>
+                Our Mission: Design Quality Products For Efficient Travel 
               </Typography>
-              <Typography variant="body1" fontWeight="500" gutterBottom>
+              <Typography variant="body1" fontWeight="500" gutterBottom sx={{mb:2}}>
                 Proin eu ante vel mauris molestie dignissim non eget nunc. Integer ac massa orci.
                 Suspendisse vulputate semper nunc eget rhoncus.
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Praesent vel faucibus ligula. Sed sit amet ipsum eget velit aliquet faucibus.
-                Maecenas et odio id turpis sollicitudin pulvinar sit amet vitae augue. Phasellus nec
-                ultricies arcu. Quisque efficitur velit sit amet bibendum molestie.
+                We're Will and Felix, co-founders of Arcido and we now live our lives on the road. We're continually moving from place to place, and using our products as we go. We've graduated from the basic gear that we started. Now we design and use high quality backpacks that solve the pain points of travel. Our mission is simple: to make the best gear that keeps us moving efficiently between cities. 
               </Typography>
             </Box>
           </Grid>
